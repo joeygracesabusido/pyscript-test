e309bd714ad1f59755b8371e67f817a89dd18a30
joeygracesabusido@gmail.com <joeygracesabusido@gmail.com>
2022-06-01 12:12:37 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+# import mysql.connector
+
+import sqlite3
+# import _mysql_connector
+
+
+
+def shuffle_array(*args):
+    pyscript.write('output','hello')
+    # result_place = Element('output').element
+    # result_place.write((float(Element('name').value).element) * float((Element('lastName').value).element))
+    
+def testing(*args,**kwargs):
+
+    result_place = Element('output')
+    result_place.write((float(Element('name').element.value)) * float((Element('lastName').element.value)))
+           
+
+# mydb = mysql.connector.connect(
+#     host="192.46.225.247",
+#     user="joeysabusido",
+#     password="Genesis@11",
+#     database="ldglobal",
+#     )
+# cursor = mydb.cursor()
+
+# def showColumns():
+#     query ='SHOW COLUMNS FROM ldglobal.payroll_computation;'
+#     cursor.execute(query)
+#     myresult = cursor.fetchall()
+
+#     for i in myresult:
+#         pyscript.write('output',i[0])
